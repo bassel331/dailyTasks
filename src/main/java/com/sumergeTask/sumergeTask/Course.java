c57498c80c981b36b6e01bcba1af72bf328bb17b
Bassel Essam <bessam@sumerge.com>
2024-08-01 02:31:02 +0300

    second task done

---
+++ src/main/java/com/sumergeTask/sumergeTask/Course.java
@@ -0,0 +1,56 @@
+package com.sumergeTask.sumergeTask;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("prototype")
+public class Course {
+   private int id;
+    private String name;
+    private String description;
+    private int credit;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", credit=" + credit +
+                '}';
+    }
+}
--- src/main/java/com/sumergeTask/sumergeTask/SumergeTaskApplication.java
+++ src/main/java/com/sumergeTask/sumergeTask/SumergeTaskApplication.java
@@ -2,13 +2,46 @@ package com.sumergeTask.sumergeTask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+import java.util.List;
 
+@SpringBootApplication
 public class SumergeTaskApplication {
 
 	public static void main(String[] args) {
 
-		SpringApplication.run(SumergeTaskApplication.class, args);
+		ApplicationContext context =SpringApplication.run(SumergeTaskApplication.class, args);
+
+		Course course1 =context.getBean(Course.class);
+		course1.setId(1);
+		course1.setName("Sumerge Task");
+		course1.setDescription("Sumerge Task description");
+		course1.setCredit(5);
+
+		Course course2 =context.getBean(Course.class);
+		course2.setId(2);
+		course2.setName("Sumerge Taskkkkk");
+		course2.setDescription("Sumerge Task descriptionnnn");
+		course2.setCredit(5);
+
+		CourseService courseService = context.getBean(CourseService.class);
+		courseService.addCourse(course1);
+		courseService.addCourse(course2);
+        List<Course> coursesAvailable =courseService.getCourses();
+		System.out.println("Courses Available now are :"+coursesAvailable);
+		System.out.println("-----------------");
+
+		courseService.deleteCourse(course1);
+		coursesAvailable=courseService.getCourses();
+		System.out.println("After deletion become:"+coursesAvailable);
+		System.out.println("-----------------");
+
+		courseService.updateCourse(course2,"cs4","desc" ,8);
+		coursesAvailable = courseService.getCourses();
+		System.out.println("After updating:"+coursesAvailable);
+		System.out.println("-----------------");
+
 	}
 
 }
