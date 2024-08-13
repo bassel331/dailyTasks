0cd60b44be2b73066c479b7488f76e4a9d969445
Bassel Essam <bessam@sumerge.com>
2024-08-13 14:31:38 +0300

    task8 done

---
+++ src/main/java/com/sumergeTask/sumergeTask/security/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.sumergeTask.sumergeTask.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+   @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+       http
+               .csrf(AbstractHttpConfigurer::disable)
+               .addFilterBefore(new ValidationFilter(), UsernamePasswordAuthenticationFilter.class)
+               .authorizeHttpRequests(authz -> authz
+                       .requestMatchers("viewAll","view/","viewPag").permitAll()
+                       .anyRequest().authenticated())
+               .httpBasic(Customizer.withDefaults());
+
+       return http.build();
+   }
+
+   @Bean
+    public UserDetailsService userDetailsService() {
+       UserDetails user = User.builder()
+               .username("user")
+               .password("$2a$12$WQmPwQSEi6A1HAENCasNGup9N/M8shxQEHmlA7km.qgIfTTjgQXW2")
+               .build();
+       UserDetails admin = User.builder()
+               .username("admin")
+               .password("$2a$12$a2SCxUeQkt2jc1kU25BOGemeL5x5HaMPbFMh9IWegdEpU6oByl8Mi")
+               .build();
+       return new InMemoryUserDetailsManager(user, admin);
+   }
+
+   @Bean
+    public PasswordEncoder passwordEncoder() {
+       return new BCryptPasswordEncoder();
+   }
+}
---
+++ src/main/java/com/sumergeTask/sumergeTask/security/ValidationFilter.java
@@ -0,0 +1,32 @@
+package com.sumergeTask.sumergeTask.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ValidationFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+
+            String validationReportHeader = request.getHeader("x-validation-report");
+
+            if (validationReportHeader == null || !validationReportHeader.equals("true")) {
+                response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+                response.getWriter().write("Missing or invalid x-validation-report header");
+                return;
+            }
+
+        filterChain.doFilter(request, response);
+    }
+
+}
